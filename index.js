5759956bcb02a5cdda62c297400f6cf77bb2f211
bayzedahmedandthe <bayzedm784@.com>
2025-01-15 22:42:29 +0600

    Added menu api patch, delete operation

--- index.js
+++ index.js
@@ -74,6 +74,39 @@ async function run() {
             const result = await menuCollection.find().toArray();
             res.send(result);
         });
+        app.post("/menu", verifyToken, verifyAdmin, async(req, res) => {
+            const menu = req.body;
+            const result = await menuCollection.insertOne(menu);
+            res.send(result);
+        });
+        app.delete("/menu/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        });
+        app.get("/menu/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.findOne(query);
+            res.send(result);
+        });
+        app.patch("/menu/:id", async(req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const updatedDoc = {
+                $set:{
+                    name: item.name,
+                    category:item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                    image: item.image
+                }
+            }
+            const result = await menuCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
         // reviews collection operations
         app.get("/reviews", async (req, res) => {
             const result = await reviewsCollection.find().toArray();
